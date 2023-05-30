4db24682606a7886d2f7dc6682488e8b04785583
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-05-30 14:38:42 +0200

    implement frontier selection using Canny edge detection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use axum::response::Html;
 use axum::routing::{get, post};
 use axum::Router;
-use local_robot_map::{CellMap, LocalMap, LocationType, MapState, RealWorldLocation};
+use local_robot_map::{Cell, CellMap, Coords, LocalMap, LocationType, MapState, RealWorldLocation};
 use local_robot_map::{Location, MaskMapState};
 
 mod polygon_handler;
@@ -77,12 +77,15 @@ fn bydistance_frontiers(map: LocalMap<CellMap>) -> LocalMap<CellMap> {
     bydistance(map).set_frontiers()
 }
 
+/// We shall take the liberty of interpreting the [`MapState::Frontier`] to be the frontier between
+/// the [`MapState::Assigned`] region and everything else. It allows us to neatly embed everything
+/// without introducing additional types.
 trait Frontiers {
     fn set_frontiers(self) -> Self;
 }
 
 impl Frontiers for LocalMap<CellMap> {
-    fn set_frontiers(self) -> Self {
+    fn set_frontiers(mut self) -> Self {
         let width: u32 = self.map().width() as u32;
         let height: u32 = self.map().height() as u32;
         let img = image::ImageBuffer::from_fn(width, height, |x, y| -> image::Luma<u8> {
@@ -94,7 +97,33 @@ impl Frontiers for LocalMap<CellMap> {
             }
         });
 
-        img.save("test-image.png").unwrap();
+        edge_detection::canny(
+            img, 1.2,  // sigma
+            0.2,  // strong threshold
+            0.01, // weak threshold
+        )
+        .as_image()
+        .to_luma8()
+        .rows()
+        .enumerate()
+        .for_each(|(row, elements)| {
+            elements.enumerate().for_each(|(col, value)| {
+                if value != &[0u8].into() {
+                    let location: RealWorldLocation = Cell::from_internal(
+                        Coords::new(col as f64, row as f64, 0.0),
+                        *self.map().offset(),
+                        *self.map().resolution(),
+                        &LocationType::Frontier,
+                    )
+                    .expect("Locations are inside the map")
+                    .location()
+                    .clone();
+                    self.map_mut()
+                        .set_location(&location, LocationType::Frontier)
+                        .expect("Location is inside the map")
+                }
+            })
+        });
 
         self
     }
