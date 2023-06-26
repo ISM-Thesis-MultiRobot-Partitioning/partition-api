0c818410f12a1cfdc32f6b2c893d99db51e0e100
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-06-26 15:51:39 +0200

    'use' all handlers at once

--- src/polygon_handler.rs
+++ src/polygon_handler.rs
@@ -2,12 +2,7 @@ mod helpers;
 mod types;
 
 mod http;
-pub use http::polygon_handler_frontiers_json;
-pub use http::polygon_handler_json;
-pub use http::polygon_handler_contours_convex_hull;
-pub use http::polygon_handler_contours_concave_hull;
-pub use http::polygon_handler_contours_polar_angular_sort;
-pub use http::polygon_handler_contours_polar_sort;
+pub use http::*;
 
 mod shared_memory;
 pub use shared_memory::polygon_handler_shm;
